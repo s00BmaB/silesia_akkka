a88a2023b257c23ae70365b4e09f304ef8a84325
bambus <kombinator1000@gmail.com>
2026-04-17 22:27:01 +0200

    JWT

--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,17 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 @api_view(['GET'])
 # @permission_classes([AllowAny]) # Jeśli chcesz, żeby to było publiczne
-@permission_classes([IsAuthenticated]) # Tylko dla zalogowanych z ważnym tokenem JWT
+@permission_classes([AllowAny])
 def hello(request):
-    return Response({"message": f"Cześć {request.user.username}, backend działa! 🚀"})
+    user = request.user
+
+    message = (
+        f"Cześć {user.username} 🚀"
+        if user.is_authenticated
+        else "Cześć niezalogowany użytkowniku 👋"
+    )
+
+    return Response({"message": message})
